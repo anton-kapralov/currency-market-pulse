8b8acbc27a0dfaae4ff7c750b8d0be9e144a4e6e
Anton Kapralov <a.e.kapralov@gmail.com>
2024-07-09 21:05:34 -0700

    [consumer] Send Trade proto message to Kafka

--- consumer/cmd/main.go
+++ consumer/cmd/main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
+	"github.com/IBM/sarama"
 	"github.com/gin-gonic/gin"
 
+	"github.com/anton-kapralov/currency-market-pulse/consumer/consuming"
 	"github.com/anton-kapralov/currency-market-pulse/consumer/http/rest"
 )
 
+func newKafkaProducer(host string, port int) sarama.AsyncProducer {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	log.Printf("Connecting to Kafka at %s", addr)
+	config := sarama.NewConfig()
+	config.Producer.Idempotent = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRoundRobinPartitioner
+	config.Net.MaxOpenRequests = 1
+	kafkaProducer, err := sarama.NewAsyncProducer([]string{addr}, config)
+	if err != nil {
+		log.Fatalf("Failed to create a Kafka producer: %s", err)
+	}
+	return kafkaProducer
+}
+
+type options struct {
+	kafka struct {
+		host string
+		port int
+	}
+}
+
 func main() {
+	var opts options
+	flag.StringVar(&opts.kafka.host, "kafka.host", "localhost", "Kafka host")
+	flag.IntVar(&opts.kafka.port, "kafka.port", 9092, "Kafka port")
+	flag.Parse()
+
+	kafkaProducer := newKafkaProducer(opts.kafka.host, opts.kafka.port)
+	consumer := consuming.NewService(kafkaProducer, "currency-trades")
+	restController := rest.NewController(consumer)
+
 	router := gin.Default()
-	router.POST("/api/trade", rest.Consume)
+	router.POST("/api/trade", restController.SaveTradeMessage)
 
 	log.Fatalln(router.Run(":8081"))
 }
--- consumer/http/rest/api.go
+++ consumer/http/rest/api.go
@@ -1,9 +1,14 @@
 package rest
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/anton-kapralov/currency-market-pulse/consumer/consuming"
+	"github.com/anton-kapralov/currency-market-pulse/proto-gen/currencymarketpb"
 )
 
 type message struct {
@@ -17,12 +22,47 @@ type message struct {
 	OriginatingCountry string  `json:"originatingCountry"`
 }
 
-func Consume(c *gin.Context) {
+type Controller interface {
+	SaveTradeMessage(c *gin.Context)
+}
+
+type controller struct {
+	consumer consuming.Service
+}
+
+func NewController(consumer consuming.Service) Controller {
+	return &controller{consumer: consumer}
+}
+
+func (c *controller) SaveTradeMessage(ctx *gin.Context) {
 	var msg message
-	if err := c.ShouldBindJSON(&msg); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := ctx.ShouldBindJSON(&msg); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	log.Printf("%+v", msg)
-	c.Status(http.StatusAccepted)
+	timePlaced, err := time.Parse("02-Jan-06 15:04:05", msg.TimePlaced)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	trade := &currencymarketpb.Trade{
+		UserId:             msg.UserId,
+		CurrencyFrom:       msg.CurrencyFrom,
+		CurrencyTo:         msg.CurrencyTo,
+		AmountSellMicros:   toMicros(msg.AmountSell),
+		AmountBuyMicros:    toMicros(msg.AmountBuy),
+		TimePlacedMs:       timePlaced.UnixMilli(),
+		OriginatingCountry: msg.OriginatingCountry,
+	}
+	if err := c.consumer.Save(trade); err != nil {
+		log.Println(err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	ctx.Status(http.StatusAccepted)
+}
+
+func toMicros(amount float64) int64 {
+	return int64(amount * 1_000_000)
 }
