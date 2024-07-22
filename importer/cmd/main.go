6e60c1edcb183642478f98ec3ff5a220bf4bf705
Anton Kapralov <a.e.kapralov@gmail.com>
2024-07-22 16:06:38 -0700

    [importer] Save trade messages to Clickhouse DB

--- importer/cmd/main.go
+++ importer/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +10,8 @@ import (
 	"os/signal"
 	"sync"
 
+	"github.com/ClickHouse/ch-go"
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/IBM/sarama"
 
 	"github.com/anton-kapralov/currency-market-pulse/importer/importing"
@@ -24,6 +27,27 @@ func newKafkaConsumerGroup(host string, port int) sarama.ConsumerGroup {
 		log.Fatalf("Error creating consumer group client: %v", err)
 	}
 	return kafkaConsumerGroup
+}
+
+func newClickhouseClient(host string, port int) *ch.Client {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	log.Printf("Connecting to Clickhouse at %s", addr)
+	ctx := context.Background()
+	c, err := ch.Dial(ctx, ch.Options{
+		Address:  addr,
+		Database: "cmp",
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to Clickhouse DB: %s", err)
+	}
+	if err := c.Ping(ctx); err != nil {
+		var exception *clickhouse.Exception
+		if errors.As(err, &exception) {
+			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		log.Fatalf("Failed to ping Clickhouse DB: %s", err)
+	}
+	return c
 }
 
 func onInterrupt(cancel context.CancelFunc) {
@@ -42,22 +66,29 @@ type options struct {
 		host string
 		port int
 	}
+	clickhouse struct {
+		host string
+		port int
+	}
 }
 
 func main() {
 	var opts options
 	flag.StringVar(&opts.kafka.host, "kafka.host", "localhost", "Kafka host")
 	flag.IntVar(&opts.kafka.port, "kafka.port", 9092, "Kafka port")
+	flag.StringVar(&opts.clickhouse.host, "clickhouse.host", "localhost", "Kafka host")
+	flag.IntVar(&opts.clickhouse.port, "clickhouse.port", 9000, "Kafka port")
 	flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	onInterrupt(cancel)
 
 	kafkaConsumerGroup := newKafkaConsumerGroup(opts.kafka.host, opts.kafka.port)
+	clickhouseClient := newClickhouseClient(opts.clickhouse.host, opts.clickhouse.port)
 
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	service := importing.NewService(kafkaConsumerGroup, "currency-trades")
+	service := importing.NewService(kafkaConsumerGroup, "currency-trades", clickhouseClient)
 	service.Start(ctx, wg)
 
 	log.Println("Ready")
--- importer/importing/service.go
+++ importer/importing/service.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/IBM/sarama"
-	"github.com/anton-kapralov/currency-market-pulse/proto-gen/currencymarketpb"
-	"google.golang.org/protobuf/proto"
 	"log"
 	"sync"
+	"time"
+
+	"github.com/ClickHouse/ch-go"
+	chproto "github.com/ClickHouse/ch-go/proto"
+	"github.com/IBM/sarama"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/anton-kapralov/currency-market-pulse/proto-gen/currencymarketpb"
 )
 
 type Service interface {
@@ -16,14 +21,16 @@ type Service interface {
 }
 
 type service struct {
-	kafkaConsumerGroup sarama.ConsumerGroup
-	topics             []string
+	kafka  sarama.ConsumerGroup
+	topics []string
+	db     *ch.Client
 }
 
-func NewService(kafkaConsumerGroup sarama.ConsumerGroup, topic string) Service {
+func NewService(kafka sarama.ConsumerGroup, topic string, db *ch.Client) Service {
 	return &service{
-		kafkaConsumerGroup: kafkaConsumerGroup,
-		topics:             []string{topic},
+		kafka:  kafka,
+		topics: []string{topic},
+		db:     db,
 	}
 }
 
@@ -34,7 +41,7 @@ func (s *service) Start(ctx context.Context, wg *sync.WaitGroup) {
 			// `Consume` should be called inside an infinite loop, when a
 			// server-side rebalance happens, the consumer session will need to be
 			// recreated to get the new claims
-			if err := s.kafkaConsumerGroup.Consume(ctx, s.topics, s); err != nil {
+			if err := s.kafka.Consume(ctx, s.topics, s); err != nil {
 				if errors.Is(err, sarama.ErrClosedConsumerGroup) {
 					return
 				}
@@ -69,9 +76,55 @@ func (s *service) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama
 				return fmt.Errorf("failed to unmarshal trade message: %s", err)
 			}
 			log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", trade, message.Timestamp, message.Topic)
+			if err := s.saveTrade(trade); err != nil {
+				log.Println(err)
+				return err
+			}
 			session.MarkMessage(message, "")
 		case <-session.Context().Done():
 			return nil
 		}
 	}
+}
+
+func (s *service) saveTrade(trade *currencymarketpb.Trade) error {
+	var (
+		userId             chproto.ColStr
+		currencyFrom       = new(chproto.ColStr).LowCardinality()
+		currencyTo         = new(chproto.ColStr).LowCardinality()
+		amountSellMicros   chproto.ColUInt64
+		amountBuyMicros    chproto.ColUInt64
+		rate               chproto.ColFloat64
+		originatingCountry = new(chproto.ColStr).LowCardinality()
+		timePlaced         = new(chproto.ColDateTime64).WithPrecision(chproto.PrecisionMilli)
+	)
+
+	userId.Append(trade.UserId)
+	currencyFrom.Append(trade.CurrencyFrom)
+	currencyTo.Append(trade.CurrencyTo)
+	amountSellMicros.Append(uint64(trade.AmountSellMicros))
+	amountBuyMicros.Append(uint64(trade.AmountBuyMicros))
+	rate.Append(float64(trade.AmountBuyMicros) / float64(trade.AmountSellMicros))
+	originatingCountry.Append(trade.OriginatingCountry)
+	timePlaced.Append(time.Unix(0, trade.TimePlacedMs*int64(time.Millisecond)))
+
+	ctx := context.Background()
+	input := chproto.Input{
+		{Name: "user_id", Data: userId},
+		{Name: "currency_from", Data: currencyFrom},
+		{Name: "currency_to", Data: currencyTo},
+		{Name: "amount_sell_micros", Data: amountSellMicros},
+		{Name: "amount_buy_micros", Data: amountBuyMicros},
+		{Name: "rate", Data: rate},
+		{Name: "originating_country", Data: originatingCountry},
+		{Name: "time_placed", Data: timePlaced},
+	}
+	q := ch.Query{
+		Body:  input.Into("trades"),
+		Input: input,
+	}
+	if err := s.db.Do(ctx, q); err != nil {
+		return fmt.Errorf("failed to save a trade in the DB: %s", err)
+	}
+	return nil
 }
